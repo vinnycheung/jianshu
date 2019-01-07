bc39166761ff2d864bb0a7ad67e0f99c84385a51
zhangshengli <zhangshengli@doctorwork.com>
2019-01-07 15:28:31 +0800

    actionCreators

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import {CSSTransition} from 'react-transition-group';
+import {actionCreators} from './store';
 import {
   HeaderWrap,
   Logo,
@@ -46,24 +47,18 @@ const Header = (props) => {
 
 const mapStateToProps = (state) => {
   return {
-    focused: state.focused
+    focused: state.header.focused
   }
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
     handleInputFocus() {
-      const action = {
-        type: 'search_focus'
-      };
-      dispatch(action);
+      dispatch(actionCreators.searchFocus());
     },
 
     handleInputBlur() {
-      const action = {
-        type: 'search_blur'
-      };
-      dispatch(action);
+      dispatch(actionCreators.searchBlur());
     }
 
   }

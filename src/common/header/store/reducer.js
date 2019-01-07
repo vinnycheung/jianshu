ec25eaf88220de89fdc9b25ae42fe1db9fccda6c
zhangshengli <zhangshengli@doctorwork.com>
2019-01-07 17:32:20 +0800

    immutable & redux-immutable & redux-thunk & axios

--- src/common/header/store/reducer.js
+++ src/common/header/store/reducer.js
@@ -1,19 +1,21 @@
 import * as constants from './constants';
+import {fromJS} from 'immutable';
 
-const defaultState = {
-  focused: false
-};
+const defaultState = fromJS({
+  focused: false,
+  list: []
+});
 
 export default (state = defaultState, action) => {
   if (action.type === constants.SEARCH_FOCUS) {
-    return {
-      focused: true
-    }
+    return state.set('focused', true);
   }
   if (action.type === constants.SEARCH_BLUR) {
-    return {
-      focused: false
-    }
+    return state.set('focused', false);
   }
+  if (action.type === constants.CHANGE_LIST) {
+    return state.set('list', action.data);
+  }
+
   return state;
 }
--- src/common/header/style.js
+++ src/common/header/style.js
@@ -89,13 +89,40 @@ export const NavSearch = styled.input.attrs({
     }
   }
 `;
+export const SearchInfo = styled.div`
+  position: absolute;
+  top: 56px;
+  left: 0;
+  width: 200px;
+  padding: 0 20px;
+  box-shadow: 0 0 8px rgba(0,0,0,.2);
+`;
+export const SearchInfoTitle = styled.div`
+  margin: 20px 0 15px 0;
+  line-height: 20px;
+  font-size: 14px;
+  color: #969696;
+`;
+export const SearchInfoSwitch = styled.span`
+  float: right;
+  font-size: 12px;
+`;
+export const SearchInfoItem = styled.a`
+  display: inline-block;
+  line-height: 20px;
+  padding: 0 5px;
+  margin: 0 10px 10px 0;
+  font-size: 12px;
+  color: #787878;
+  border: 1px solid #ddd;
+  border-radius: 3px;
+`;
 export const Addition = styled.div`
   position: absolute;
   top: 0;
   right: 0;
   height: 56px;
 `;
-
 export const Button = styled.div`
   float: right;
   margin-top: 9px;
